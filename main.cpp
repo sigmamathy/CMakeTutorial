ea3140a94aa8b702b51387b34461d4c50c8286f3
sigmamathy <makgsum@gmail.com>
2022-12-22 23:33:15 +0800

    Changes mylib to optional

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <NewTestConfig.h>
+#ifdef USE_MYLIB
+#include <math.hpp>
+#endif
 
 int main(int argc, char** argv)
 {
     std::cout << argv[0] << " Version " << NEWTEST_VERSION_MAJOR << '.' << NEWTEST_VERSION_MINOR << '\n';
+#ifdef USE_MYLIB
+    std::cout << "Area = " << mylib::HeronFormula(3, 4, 5) << '\n';
+#endif
 
     glfwInit();
     GLFWwindow* window = glfwCreateWindow(1600, 900, "Hello World", nullptr, nullptr);
